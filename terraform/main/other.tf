8b6a9451a51c4ddd1501c71161785941a69ebd41
Hien Vuong <hienvuong0403@gmail.com>
2026-09-25 12:06:23 +0700

    test remove annotatation

--- terraform/main/other.tf
+++ terraform/main/other.tf
@@ -3,14 +3,14 @@ data "azurerm_user_assigned_identity" "keda_backend" {
   resource_group_name = "rg-${local.project_name}"
 }
 
-resource "kubernetes_annotations" "keda_operator_sa" {
-  api_version = "v1"
-  kind        = "ServiceAccount"
-  metadata {
-    name      = "keda-operator"
-    namespace = "kube-system"
-  }
-  annotations = {
-    "azure.workload.identity/client-id" = data.azurerm_user_assigned_identity.keda_backend.client_id
-  }
-}
+# resource "kubernetes_annotations" "keda_operator_sa" {
+#   api_version = "v1"
+#   kind        = "ServiceAccount"
+#   metadata {
+#     name      = "keda-operator"
+#     namespace = "kube-system"
+#   }
+#   annotations = {
+#     "azure.workload.identity/client-id" = data.azurerm_user_assigned_identity.keda_backend.client_id
+#   }
+# }
